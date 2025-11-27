ad93de3aedc594faf8a3e1f5f113bb5d74ac7f62
wonsechan <chxnroze@gmail.com>
2025-11-27 22:48:03 +0900

    dbc

--- src/DB/DBC.java
+++ src/DB/DBC.java
@@ -1,5 +1,25 @@
 package DB;
 
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBC {
+    public static final String databaseDriver = "com.mysql.cj.jdbc.Driver";
+    public static final String databaseUrl = "jdbc:mysql://localhost:3306/ScheduleManagement?serverTimezone=UTC&characterEncoding=UTF8&useSSL=false";
+    public static final String databaseUser = "root";
+    public static final String databasePassword = "개인비밀번호";
     
+    public static Connection connection = null;
+    
+    public static void main(String[] args) {
+    	connect();
+    	close();
+    	
+    	
+    	
+    }
+
+	
 }
