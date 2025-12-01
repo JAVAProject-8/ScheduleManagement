bb3157d5f283258841fc84eaffa15faf3d9c9b64
Marble2468 <msy963@naver.com>
2025-12-01 19:46:48 +0900

    메모 객체 속성 추가

--- src/DB/Memo.java
+++ src/DB/Memo.java
@@ -1,5 +1,14 @@
 package DB;
 
 public class Memo {
-
+    // 메모 id
+    int memo_id;
+    // 그룹 id
+    int group_id;
+    // 작성자 id (사용자 id)
+    String writer_id;
+    // 내용
+    String content;
+    // 작성일시
+    String created_at;
 }
