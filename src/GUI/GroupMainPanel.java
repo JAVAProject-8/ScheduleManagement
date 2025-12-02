5960527e8b7decd7babf50d281fbc8c3b104d5d7
Jeon-Seho <ho10080@naver.com>
2025-12-02 12:15:34 +0900

    Feat: Group Main(test)

---
+++ src/GUI/GroupMainPanel.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.EmptyBorder;
+
+public class GroupMainPanel extends JPanel {
+	
+	public GroupMainPanel() {
+		
+	}
+}
