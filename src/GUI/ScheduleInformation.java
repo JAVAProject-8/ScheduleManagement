ff488fcd6be7625c375caa6698e82a3f47925520
Jeon-Seho <ho10080@naver.com>
2025-12-02 12:12:04 +0900

    Fix: Main Class, Look And Feel

--- src/GUI/ScheduleInformation.java
+++ src/GUI/ScheduleInformation.java
@@ -1,7 +1,18 @@
 package GUI;
 
+import javax.swing.*;
+
 public class ScheduleInformation {
+	// 메인 메소드
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());	// Java 기본 Look And Feel 설정
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 		new LoginFrame();
+		//new MainFrame();
 	}
 }
