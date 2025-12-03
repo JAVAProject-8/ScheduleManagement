b2a95651946f3a8d5c4ba7507c84e0ddb76dd6c8
ghdrb1246 <hon2002226@gmail.com>
2025-12-03 18:00:59 +0900

    Fix : GUITest

--- src/GUI/Panel/test/GUITest.java
+++ src/GUI/Panel/test/GUITest.java
@@ -10,7 +10,6 @@ public class GUITest {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());	// Java 기본 Look And Feel 설정
-            
         }
         catch(Exception e) {}
 
