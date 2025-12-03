b02881d9bbfbf2c11c41a70c002dafd8b4ceb3e2
ghdrb1246 <hon2002226@gmail.com>
2025-12-03 18:01:17 +0900

    Fix : TestDAO

--- src/GUI/Panel/test/TestDAO.java
+++ src/GUI/Panel/test/TestDAO.java
@@ -1,31 +1,40 @@
 package GUI.Panel.test;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import DB.Schedule;
 
 public class TestDAO {
     public ArrayList<Schedule> getPersonalSchedule(String userId) {
-        ArrayList<Schedule> list = new ArrayList<>();
-
+        ArrayList<Schedule> list = new ArrayList<>(); 
+        
+        // "2025-11-27 09:00"
+        // "2025-11-27 12:15"
         list.add(new Schedule(
                 1, userId, 0, "자료구조 강의", "일",
-                "2025-11-27 09:00",
-                "2025-11-27 12:15"
+                LocalDateTime.of(2025, 11, 27, 9, 0),
+                LocalDateTime.of(2025, 11, 27, 12, 15)
         ));
+        // "2025-11-25 13:00",
+        // "2025-11-25 14:00"
         list.add(new Schedule(
                 2, userId, 0, "헬스장", "일",
-                "2025-11-25 13:00",
-                "2025-11-25 14:00"
+                LocalDateTime.of(2025, 11, 25, 13, 0),
+                LocalDateTime.of(2025, 11, 25, 14, 0)
         ));
+        // "2025-11-26 15:00",
+        // "2025-11-26 16:00"
         list.add(new Schedule(
                 3, userId, 0, "팀 프로젝트 회의", "일",
-                "2025-11-26 15:00",
-                "2025-11-26 16:00"
+                LocalDateTime.of(2025, 11, 26, 15, 0),
+                LocalDateTime.of(2025, 11, 26, 16, 0)
         ));
+        // "2025-11-28 11:00",
+        // "2025-11-28 12:00"
         list.add(new Schedule(
                 4, userId, 0, "자바 과제", "일",
-                "2025-11-28 11:00",
-                "2025-11-28 12:00"
+                LocalDateTime.of(2025, 11, 28, 11, 0),
+                LocalDateTime.of(2025, 11, 28, 12, 0)
         ));
 
         return list;
@@ -35,9 +44,7 @@ public class TestDAO {
         ArrayList<String> glist = new ArrayList<>();
         glist.add("GA");
         glist.add("GB");
-        glist.add("GC");
-        glist.add("GD");
-
+        
         return glist;
     }
 
@@ -45,25 +52,54 @@ public class TestDAO {
         ArrayList<Schedule> list = new ArrayList<>();
         
         if (groupName.equals("GA")) {
-            list.add(new Schedule(
-                        1, "user1", 1, "회의", "일",
-                        "2025-12-01 10:00", "2025-12-01 11:00")
-                );
-            list.add(new Schedule(
-                        2, "user2", 1, "설계 작업", "일",
-                        "2025-12-02 14:00", "2025-12-02 15:00")
-                );
-            list.add(new Schedule(
-                        3, "user1", 1, "보고서", "일",
-                        "2025-12-03 09:00", "2025-12-03 10:00")
-                );
+                list.add(new Schedule(
+                        1, 
+                        "user1", 
+                        1, 
+                        "회의",
+                         "일",
+                        LocalDateTime.of(2025, 12, 01, 10, 0),
+                        LocalDateTime.of(2025, 12, 02, 11, 0)
+                ));
+                list.add(new Schedule(
+                        2,
+                        "user2", 
+                        1, 
+                        "설계 작업", 
+                        "일",
+                        LocalDateTime.of(2025, 12, 02, 14, 0),
+                        LocalDateTime.of(2025, 12, 02, 15, 0)
+                ));
+                list.add(new Schedule(
+                        3, 
+                        "user1", 
+                        1, 
+                        "보고서",
+                        "일",
+                        LocalDateTime.of(2025, 12, 03, 9, 0),
+                        LocalDateTime.of(2025, 12, 03, 10, 0)
+                ));
         }
 
         else if (groupName.equals("GB")) {
-            list.add(new Schedule(4, "kim", 2, "스터디", "일",
-                                    "2025-12-01 15:00", "2025-12-01 17:00"));
-            list.add(new Schedule(5, "lee", 2, "자료 준비", "일",
-                                    "2025-12-02 13:00", "2025-12-02 15:00"));
+                list.add(new Schedule(
+                        4, 
+                        "kim", 
+                        2, 
+                        "스터디", 
+                        "일",
+                        LocalDateTime.of(2025, 12, 01, 15, 0),
+                        LocalDateTime.of(2025, 12, 01, 17, 0)
+                ));
+                list.add(new Schedule(
+                        5, 
+                        "lee", 
+                        2, 
+                        "자료 준비", 
+                        "일",
+                        LocalDateTime.of(2025, 12, 02, 13, 0),
+                        LocalDateTime.of(2025, 12, 02, 15, 0)
+                ));
         }
         else {
                 list = null;
