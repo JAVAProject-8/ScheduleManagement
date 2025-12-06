2eb78090425704b908a02a9ac0bbb3f6364d881a
Jeon-Seho <ho10080@naver.com>
2025-12-06 18:36:13 +0900

    Fix: Group Main(Database Access)

--- src/GUI/Panel/GroupMainPanel.java
+++ src/GUI/Panel/GroupMainPanel.java
@@ -7,14 +7,20 @@ import DB.Group;
 import DB.Member;
 import DB.Memo;
 import DB.SDAO;
+import DB.Schedule;
 
 import javax.swing.*;
 import javax.swing.table.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.time.format.DateTimeFormatter;
+
 import javax.swing.border.EmptyBorder;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Vector;
+import java.time.*;
 
 public class GroupMainPanel extends JPanel implements ActionListener {
 	JComboBox<String> groupComboBox;	// 그룹 선택을 위한 콤보박스
@@ -38,28 +44,9 @@ public class GroupMainPanel extends JPanel implements ActionListener {
 		setLayout(new BorderLayout());
 		user = _u;
 		
-		groups = SDAO.getInstance().getMyGroups(user.getID());	// 사용자 아이디를 인수로 DAO 객체에서 그룹 ArrayList 반환
-		
-		if(groups.size() == 0) {	// 사용자가 가입되어있는 그룹이 없는 경우
-			JLabel impormationLabel = new JLabel("초대 코드를 입력하여 그룹에 가입해주세요.", JLabel.CENTER);
-			impormationLabel.setFont(new Font("Malgun Gothic", Font.BOLD, 30));
-			add(impormationLabel, BorderLayout.CENTER);
+		if(setGroup()) {
 			return;
 		}
-		
-		selectedGroup = groups.get(0);
-		
-		Vector<String> items = new Vector<>();	// Vector 선언
-		
-		for(int i = 0; i < groups.size(); i++) {
-			items.add(groups.get(i).getGroupName());	// 그룹 이름을 Vector에 추가
-		}
-		comboBoxModel = new DefaultComboBoxModel<>(items);	// Vector로 모델 생성
-		groupComboBox = new JComboBox<>(comboBoxModel);		// 생성한 모델로 콤보박스 생성
-		// 그룹 가입 시, 탈퇴 시 콤보박스 재선언 필요
-		
-		groupComboBox.setBackground(Color.WHITE);
-		groupComboBox.setFont(new Font("Malgun Gothic", Font.BOLD, 20));
 		
 		setGroupInfo();
 		
@@ -71,7 +58,6 @@ public class GroupMainPanel extends JPanel implements ActionListener {
 		JPanel leftPanel = new JPanel();
 		leftPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 		leftPanel.add(groupComboBox);
-		leftPanel.add(groupNameLabel);
 		leftPanel.add(groupInviteCodeLabel);
 		
 		// 상단 우측 패널
@@ -119,30 +105,29 @@ public class GroupMainPanel extends JPanel implements ActionListener {
 		tableModel.addColumn("이름");
 		tableModel.addColumn("업무");
 		
-		// 테스트 데이터
-//		tableModel.addRow(new Object[] {"홍길동", "프로그래밍"});
-//		tableModel.addRow(new Object[] {"강감찬", "데이터베이스"});
-//		tableModel.addRow(new Object[] {"유관순", "없음"});
-//		tableModel.addRow(new Object[] {"이순신", "프로젝트 매니저"});
-//		tableModel.addRow(new Object[] {"서희", "보고서 및 발표"});
-		
 		setGroupTask();
 		
 		// 테이블 설정
 		groupMemberTable = new JTable(tableModel);
 		groupMemberTable.setRowHeight(20);	// 행 높이 설정
 		groupMemberTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);	// 테이블 자동 크기 조절 끄기
-		groupMemberTable.getColumnModel().getColumn(0).setPreferredWidth(70);	// 첫 번째 열 너비 설정
-		groupMemberTable.getColumnModel().getColumn(1).setPreferredWidth(350);	// 두 번째 열 너비 설정
+		groupMemberTable.getColumnModel().getColumn(0).setPreferredWidth(60);	// 첫 번째 열 너비 설정
+		groupMemberTable.getColumnModel().getColumn(1).setPreferredWidth(280);	// 두 번째 열 너비 설정
 		groupMemberTable.getTableHeader().setReorderingAllowed(false);	// 열 순서 변경 금지 처리
 		groupMemberTable.getTableHeader().setResizingAllowed(false);	// 열 너비 변경 금지 처리
 		
 		groupMemberTable.addMouseListener(new MouseListener() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				int selectedIndex = groupMemberTable.getSelectedRow();	// 선택한 행의 인덱스를 가져옴
+				
+				int selectedModelIndex = groupMemberTable.convertRowIndexToModel(selectedIndex);	// 테이블 모델의 인덱스로 변환
+				Member selectedMember = members.get(selectedModelIndex);
+				User selectedUser = SDAO.getInstance().getUserInfo(selectedMember.getUserId());
+				
 				// 특정 행 더블 클릭 시 그룹원 상세 정보 출력
 				Window mainFrame = SwingUtilities.getWindowAncestor(groupMemberTable);
-				//new GroupMemberInfoDialog((JFrame)mainFrame, "상세 정보", "선택한 그룹원 User 객체", "선택한 그룹원 Member 객체");
+				new GroupMemberInfoDialog((JFrame)mainFrame, "상세 정보", selectedUser, selectedMember);
 			}
 			
 			@Override
@@ -160,7 +145,8 @@ public class GroupMainPanel extends JPanel implements ActionListener {
 		
 		// 우측 패널
 		JPanel groupMemberPanel = new JPanel();
-		groupMemberPanel.add(new JScrollPane(groupMemberTable));
+		groupMemberPanel.setLayout(new BorderLayout());
+		groupMemberPanel.add(new JScrollPane(groupMemberTable), BorderLayout.CENTER);
 		groupMemberPanel.setBorder(new EmptyBorder(0, 10, 20, 20));
 		
 		groupComboBox.addActionListener(this);
@@ -194,15 +180,20 @@ public class GroupMainPanel extends JPanel implements ActionListener {
 			}
 			
 			// 메모 객체 생성
-			//Memo memo = new Memo("그룹 아이디", user.getID, memoText, LocalDateTime.now());
+			Memo memo = new Memo(selectedGroup.getGroupId(), user.getID(), memoText, LocalDateTime.now());
 			
 			// 사용자의 입력을 가져와 DB에 전달
-			//boolean result = SDAO.getInstance().insertMemo(memo);
+			boolean result = SDAO.getInstance().insertMemo(memo);
 			
 			// 전달 성공 시 TextArea 갱신
-			memoArea.append(memoText + "\n");	// 이름(현재시간): 메시지 형식으로 추가해야 함
-			
-			// 전달 실패 시 JOptionPane
+			if(result) {
+				JOptionPane.showMessageDialog(null, "메모 입력 성공", "Information", JOptionPane.PLAIN_MESSAGE);
+				setGroupMemo();
+			}
+			// 전달 실패 시
+			else {
+				JOptionPane.showMessageDialog(null, "메모 입력 실패. 다시 시도해 주세요.", "Warning", JOptionPane.WARNING_MESSAGE);
+			}
 			
 			memoField.setText("");	// 입력 필드 초기화
 		}
@@ -218,38 +209,74 @@ public class GroupMainPanel extends JPanel implements ActionListener {
 			boolean result = SDAO.getInstance().updateTask(user.getID(), selectedGroup.getGroupId(), task);
 			
 			if(result) {
-				JOptionPane.showMessageDialog(null, "일정 입력 성공", "Information", JOptionPane.PLAIN_MESSAGE);
+				JOptionPane.showMessageDialog(null, "업무 입력 성공", "Information", JOptionPane.PLAIN_MESSAGE);
+				setGroupTask();
 			}
 			else {
-				JOptionPane.showMessageDialog(null, "일정 입력 실패. 다시 시도해 주세요.", "Warning", JOptionPane.WARNING_MESSAGE);
+				JOptionPane.showMessageDialog(null, "업무 입력 실패. 다시 시도해 주세요.", "Warning", JOptionPane.WARNING_MESSAGE);
 			}
 		}
 	}
 	
-	// 처음 생성자 실행 시, 콤보박스 선택 시 실행됨
-	public void setGroupInfo() {
-		// 그룹명, 초대코드
-		groupNameLabel = new JLabel("\t그룹명: " + selectedGroup.getGroupName());
-		groupNameLabel.setFont(new Font("Malgun Gothic", Font.BOLD, 20));
+	public boolean setGroup() {
+		groups = SDAO.getInstance().getMyGroups(user.getID());	// 사용자 아이디를 인수로 DAO 객체에서 그룹 ArrayList 반환
 		
-		groupInviteCodeLabel = new JLabel("/ 초대 코드: " + selectedGroup.getInviteCode());
+		if(groups.size() == 0) {	// 사용자가 가입되어있는 그룹이 없는 경우
+			JLabel impormationLabel = new JLabel("초대 코드를 입력하여 그룹에 가입해주세요.", JLabel.CENTER);
+			impormationLabel.setFont(new Font("Malgun Gothic", Font.BOLD, 30));
+			add(impormationLabel, BorderLayout.CENTER);
+			return true;
+		}
+		
+		selectedGroup = groups.get(0);
+		
+		Vector<String> items = new Vector<>();	// Vector 선언
+		
+		for(int i = 0; i < groups.size(); i++) {
+			items.add(groups.get(i).getGroupName());	// 그룹 이름을 Vector에 추가
+		}
+		comboBoxModel = new DefaultComboBoxModel<>(items);	// Vector로 모델 생성
+		groupComboBox = new JComboBox<>(comboBoxModel);		// 생성한 모델로 콤보박스 생성
+		// 그룹 가입 시, 탈퇴 시 콤보박스 재선언 필요
+		
+		groupComboBox.setBackground(Color.WHITE);
+		groupComboBox.setFont(new Font("Malgun Gothic", Font.BOLD, 20));
+		
+		return false;
+	}
+	
+	// 그룹 정보 설정
+	public void setGroupInfo() {
+		groupInviteCodeLabel = new JLabel("\t초대 코드: " + selectedGroup.getInviteCode());
 		groupInviteCodeLabel.setFont(new Font("Malgun Gothic", Font.BOLD, 20));
 	}
 	
+	// 그룹 메모 설정
 	public void setGroupMemo() {
-		// 메모
-		memos = SDAO.getInstance().getMemosByGroupId(selectedGroup.getGroupId());
-		
 		memoArea.setText("");
 		
-		for(int i = 0; i <= memos.size(); i++) {
-			//String line = memos.get(i).getWriterId() + "(" + memos.get(i).getCreatedAt() + "): " + memos.get(i).getContent();
-			//memoArea.append(line + "\n");
+		memos = SDAO.getInstance().getMemosByGroupId(selectedGroup.getGroupId());
+		
+		// DB에서 전달받은 ArrayList를 xxx을 기준으로 하여 정렬
+		Collections.sort(memos, new Comparator<Memo>() {
+			@Override
+			public int compare(Memo m1, Memo m2) {
+				return m1.getCreatedAt().compareTo(m2.getCreatedAt());
+			}
+		});
+		
+		for(int i = 0; i < memos.size(); i++) {
+			User otherUser = SDAO.getInstance().getUserInfo(memos.get(i).getWriterId());	// ID를 매개변수로 User 객체를 가져옴
+			
+			String line = otherUser.getName() + "("
+					+ memos.get(i).getCreatedAt().format(DateTimeFormatter.ofPattern("yy-MM-dd HH:mm")) + "): "
+					+ memos.get(i).getContent();
+			memoArea.append(line + "\n");
 		}
 	}
 	
+	// 그룹 업무 설정
 	public void setGroupTask() {
-		// 그룹 ID로 Member ArrayList를 반환받아 표에 삽입
 		members = SDAO.getInstance().getMembersByGroupId(selectedGroup.getGroupId());
 		tableModel.setRowCount(0);
 		
